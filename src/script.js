8bd6cfabc1099bd4ad43783f22327b0d419a74d9
Rafael Porto Annunciato <84384192+RafaelPA13@users.noreply.github.com>
2024-03-19 20:04:50 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -2,91 +2,91 @@
 const chaveDaAPI = "50370f8fc277225beccc7b9ab548f30a";
 
 //Importe de componentes
-const barraDePesquisa = document.getElementById('barraPesquisa')
-const botaoPesquisa = document.getElementById('btnPesquisa')
+const barraDePesquisa = document.getElementById("barraPesquisa");
+const botaoPesquisa = document.getElementById("btnPesquisa");
 
-const elementoCidade = document.getElementById('cidade')
-const elementoPais = document.getElementById('pais')
-const iconeDeTemperatura = document.getElementById('iconeTemperatura')
-const elementoTemperatura = document.getElementById('temperatura')
-const condicaoDoTempo = document.getElementById('condicaoTempo')
-const elementoUmidade = document.querySelector('#umidade span')
-const elementoVento = document.querySelector('#vento span')
+const elementoCidade = document.getElementById("cidade");
+const elementoPais = document.getElementById("pais");
+const iconeDeTemperatura = document.getElementById("iconeTemperatura");
+const elementoTemperatura = document.getElementById("temperatura");
+const condicaoDoTempo = document.getElementById("condicaoTempo");
+const elementoUmidade = document.querySelector("#umidade span");
+const elementoVento = document.querySelector("#vento span");
 
-//Ler as capitais do mundo e imprimir a temperatura 
+//Ler as capitais do mundo e imprimir a temperatura
 const capitais = [
-    {nome: 'Londres', bandeira: 'https://flagsapi.com/GB/flat/64.png'},
-    {nome: 'Paris', bandeira: 'https://flagsapi.com/FR/flat/64.png'},
-    {nome: 'Pequim', bandeira: 'https://flagsapi.com/CN/flat/64.png'},
-    {nome: 'Madrid', bandeira: 'https://flagsapi.com/ES/flat/64.png'},
-    {nome: 'Milão', bandeira: 'https://flagsapi.com/IT/flat/64.png'},
-    {nome: 'Seul', bandeira: 'https://flagsapi.com/KR/flat/64.png'},
-    {nome: 'Nova York', bandeira: 'https://flagsapi.com/US/flat/64.png'},
-    {nome: 'Amsterdam', bandeira: 'https://flagsapi.com/NL/flat/64.png'},
-    {nome: 'Tóquio', bandeira: 'https://flagsapi.com/JP/flat/64.png'},
-    {nome: 'Brasília', bandeira: 'https://flagsapi.com/BR/flat/64.png'}
-]
+  { nome: "Londres", bandeira: "GB" },
+  { nome: "Paris", bandeira: "FR" },
+  { nome: "Pequim", bandeira: "CN" },
+  { nome: "Madrid", bandeira: "ES" },
+  { nome: "Milão", bandeira: "IT" },
+  { nome: "Seul", bandeira: "KR" },
+  { nome: "Nova York", bandeira: "US" },
+  { nome: "Amsterdam", bandeira: "NL" },
+  { nome: "Tóquio", bandeira: "JP" },
+  { nome: "Brasília", bandeira: "BR" },
+];
 
-capitais.map(capital => {
-    const div = document.querySelector('ul')
-    const btn = document.createElement('button')
-    const h2 = document.createElement('h2')
-    const img = document.createElement('img')
+capitais.map((capital) => {
+  const ul = document.querySelector("ul");
+  const li = document.createElement("li");
+  const h2 = document.createElement("h2");
+  const img = document.createElement("img");
 
-    img.src = capital.bandeira
-    h2.innerHTML = capital.nome
-    img.value = capital.nome
-    h2.value = capital.nome
-    btn.value = capital.nome
+  img.src = `https://flagsapi.com/${capital.bandeira}/flat/64.png`;
+  h2.textContent = capital.nome;
 
-    btn.appendChild(img)
-    btn.appendChild(h2)
-    div.appendChild(btn)
+  li.appendChild(img);
+  li.appendChild(h2);
+  ul.appendChild(li)
 
-    btn.addEventListener('click', (e)=>{
-        e.preventDefault()
-        const cidade = e.target.value
-        mostrarDados(cidade)
-    })
-})
+  li.addEventListener("click", () => {mostrarDados(capital.nome);});
+});
 
 //Função para pegar os dados da api
-async function pegarDados(cidade){
-    const urlDaAPI = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${chaveDaAPI}&lang=pt_br`
-    const res = await fetch(urlDaAPI)
-    const data = await res.json()
-    
-    return data
+async function pegarDados(cidade) {
+  const urlDaAPI = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${chaveDaAPI}&lang=pt_br`;
+  const res = await fetch(urlDaAPI);
+  const data = await res.json();
+  return data;
 }
 
 //Função que imprime os dados coletados na tela do usuário
-async function mostrarDados(cidade){
-    try {
-        const data = await pegarDados(cidade)
-    
-        elementoCidade.innerText = data.name
-        elementoPais.setAttribute('src', `https://flagsapi.com/${data.sys.country}/flat/64.png`)
-        iconeDeTemperatura.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
-        elementoTemperatura.innerText = `${parseInt(data.main.temp)}°C`
-        condicaoDoTempo.innerText = data.weather[0].description
-        elementoUmidade.innerText = `${data.main.humidity}%`
-        elementoVento.innerText = `${parseInt(data.wind.speed)}km/h`        
-    } catch (error) {
-        console.log('A forma como você digitou o nome da cidade pode estar errada, tente escrevê-la novamente.')
-    }
+async function mostrarDados(cidade) {
+  try {
+    const data = await pegarDados(cidade);
+
+    elementoCidade.innerText = data.name;
+    elementoPais.setAttribute(
+      "src",
+      `https://flagsapi.com/${data.sys.country}/flat/64.png`
+    );
+    iconeDeTemperatura.setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
+    elementoTemperatura.innerText = `${parseInt(data.main.temp)}°C`;
+    condicaoDoTempo.innerText = data.weather[0].description;
+    elementoUmidade.innerText = `${data.main.humidity}%`;
+    elementoVento.innerText = `${parseInt(data.wind.speed)}km/h`;
+  } catch (error) {
+    console.log(
+      "A forma como você digitou o nome da cidade pode estar errada, tente escrevê-la novamente."
+    );
+  }
 }
 
 //Funcionalidade do botão
-botaoPesquisa.addEventListener('click', (e)=>{
-    e.preventDefault()
-    const cidade = barraDePesquisa.value
-    mostrarDados(cidade)
-})
+botaoPesquisa.addEventListener("click", (e) => {
+  e.preventDefault();
+  const cidade = barraDePesquisa.value;
+  mostrarDados(cidade);
+});
 
 //Permitindo dar enter na barra de pesquisa
-barraDePesquisa.addEventListener('keyup', (e)=>{
-    if(e.code == 'Enter'){
-        const cidade = e.target.value
-        mostrarDados(cidade)
-    }
-})
+barraDePesquisa.addEventListener("keyup", (e) => {
+  if (e.code == "Enter") {
+    const cidade = e.target.value;
+    mostrarDados(cidade);
+  }
+});
